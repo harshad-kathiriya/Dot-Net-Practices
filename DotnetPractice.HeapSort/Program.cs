f5a6e727922af3a38604f7c4500cf5445450031b
Harshad <harshad.kathiriya@gmail.com>
2022-01-28 10:06:23 +0530

    Heap Sort

---
+++ DotnetPractice.HeapSort/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace DotnetPractice.HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {12, 11, 13, 5, 6, 7};
+            int n = arr.Length;
+            Console.WriteLine("Array"); 
+            PrintArray(arr);
+            Sort(arr);
+            Console.WriteLine("Sorted Array"); 
+            PrintArray(arr);
+
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if(l < n && arr[l] > arr[largest])
+            {
+                largest = l;
+            }
+
+            if(r < n && arr[r] > arr[largest])
+            {
+                largest = r;
+            }
+
+            if(largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                heapify(arr,n,largest);
+            }
+
+        }
+
+        static void Sort(int[] arr) 
+        {
+            int n = arr.Length;
+
+            for(int i = n/2-1; i >= 0 ; i--)
+            {
+                heapify(arr, n , i);
+            }
+
+            for(int i = n - 1; i > 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                heapify(arr, i , 0);
+            }
+        }     
+
+
+    }
+}
